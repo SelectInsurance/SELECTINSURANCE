072fbd6bcc7bff30ed9a61cb01e4435b3cb5d04f
Selectinsurance <Selectinsurance210@gmail.com>
2021-07-19 17:19:09 -0500

    Limpiando codigo

--- app/controllers/ControllerManagment.php
+++ app/controllers/ControllerManagment.php
@@ -28,40 +28,6 @@ class ControllerManagment extends Pather
     public function ControllerManagmentCargaVideos()
     {
         require_once 'app/views/assets/NavAgente.php';
-
-        //if (isset($_POST['btnUploadVideoAnico']) && !empty($_POST['NombreVideoAnico'])) {
-        //    $RutaTemporal = $_FILES['Anico']['tmp_name'];
-        //    $NombreArchivo = $_FILES['Anico']['name'];
-        //    $TamañoArchivo = $_FILES['Anico']['size'];
-        //    $TipoArchivo = $_FILES['Anico']['type'];
-        //
-        //
-        //    $subirVideos = new SubidaArchivos($_POST['NombreVideoAnico'], $RutaTemporal, $NombreArchivo, $TamañoArchivo, $TipoArchivo, 'VideosAnico');
-        //    $mensaje = $subirVideos->SubidaValidando('app/views/assets/videos/videos_vida/anico/');
-        //} elseif (isset($_POST['btnUploadVideoAmeritas']) && !empty($_POST['NombreVideoAmeritas'])) {
-        //    $RutaTemporal = $_FILES['Anico']['tmp_name'];
-        //    $NombreArchivo = $_FILES['Anico']['name'];
-        //    $TamañoArchivo = $_FILES['Anico']['size'];
-        //    $TipoArchivo = $_FILES['Anico']['type'];
-        //
-        //
-        //    $subirVideos = new SubidaArchivos($_POST['NombreVideoAmeritas'], $RutaTemporal, $NombreArchivo, $TamañoArchivo, $TipoArchivo, 'VideosAmeritas');
-        //    $mensaje = $subirVideos->SubidaValidando('app/views/assets/videos/videos_vida/ameritas/');
-        //} elseif (isset($_POST['btnUploadVideoNationalLife']) && !empty($_POST['NombreVideoNationalLife'])) {
-        //    $RutaTemporal = $_FILES['Anico']['tmp_name'];
-        //    $NombreArchivo = $_FILES['Anico']['name'];
-        //    $TamañoArchivo = $_FILES['Anico']['size'];
-        //    $TipoArchivo = $_FILES['Anico']['type'];
-        //
-        //
-        //    $subirVideos = new SubidaArchivos($_POST['NombreVideoNationalLife'], $RutaTemporal, $NombreArchivo, $TamañoArchivo, $TipoArchivo, 'NationalLife');
-        //    $mensaje = $subirVideos->SubidaValidando('app/views/assets/videos/videos_vida/national_life/');
-        //}
-
-
-
-
-
         require_once 'app/views/pages/managment/modulos/CargaVideos.php';
     }
 
