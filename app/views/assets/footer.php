f57890928b34338aeac533510fb26f0b5ecbb558
Selectinsurance <Selectinsurance210@gmail.com>
2021-07-21 17:19:34 -0500

    tratando de cambiar el color del icono de whatsapp

--- app/views/assets/footer.php
+++ app/views/assets/footer.php
@@ -1,9 +1,10 @@
 <a href="https://api.whatsapp.com/send?phone=+17862032700%22" class="btn-wsp" target="__blank">
-    <i class="fab fa-whatsapp fa-2x animate__animated animate__zoomIn ColorTextoOscuro" role="status" aria-hidden="true"></i>
+        <i class="fab fa-whatsapp fa-2x animate__animated animate__zoomIn ColorTextoOscuro" role="status" aria-hidden="true"></i>
 </a>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="app/views/js/calendario/evo-calendar.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
+
 </html>
