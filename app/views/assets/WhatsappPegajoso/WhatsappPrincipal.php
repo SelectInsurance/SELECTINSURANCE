6b980ef28953148c0e3e9cc361d48b33384d35d7
Selectinsurance <Selectinsurance210@gmail.com>
2021-07-26 10:29:14 -0500

    Cambiando Icono de Whatsapp

--- app/views/assets/WhatsappPegajoso/WhatsappPrincipal.php
+++ app/views/assets/WhatsappPegajoso/WhatsappPrincipal.php
@@ -1,3 +1,3 @@
 <a href="https://api.whatsapp.com/send?phone=+17862032700%22" class="btn-wsp" target="__blank">
-        <i class="fab fa-whatsapp fa-2x animate__animated animate__zoomIn ColorTextoOscuro" role="status" aria-hidden="true"></i>
+        <i class="fab fa-whatsapp animate__animated animate__zoomIn" role="status" aria-hidden="true"></i>
 </a>
