b5beb41c88b32cfb6a89291ed126e24373bc349e
Selectinsurance <Selectinsurance210@gmail.com>
2021-07-28 13:49:13 -0500

    Agregando icono personalizado

--- app/views/assets/WhatsappPegajoso/WhatsappPrincipal.php
+++ app/views/assets/WhatsappPegajoso/WhatsappPrincipal.php
@@ -1,3 +1,12 @@
-<a href="https://api.whatsapp.com/send?phone=+17862032700%22" class="btn-wsp" target="__blank">
-        <i class="fab fa-whatsapp animate__animated animate__zoomIn" role="status" aria-hidden="true"></i>
-</a>
+<blockquote>
+	<a href="https://api.whatsapp.com/send?phone=+17862032700%22" target="__blank">
+		<p>
+			<span class="fab fa-whatsapp fa-7x btn-wsp"></span>
+			<span class="fondo-text-wsp">
+				<blockquote class="texto-wsp">
+					<h2><b>Chatea</b></h2><br>Con nosotros
+				</blockquote>
+			</span>
+		</p>
+	</a>
+</blockquote>
