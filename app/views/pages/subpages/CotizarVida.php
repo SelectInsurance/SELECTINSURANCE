234a257b71acf4c5c71a55d9ebd46e2f6e5805e7
Selectinsurance <Selectinsurance210@gmail.com>
2021-08-13 15:26:44 -0500

    Agregando cambios a la pagina de cotizador

--- app/views/pages/subpages/CotizarVida.php
+++ app/views/pages/subpages/CotizarVida.php
@@ -1,40 +1,64 @@
-<div class="container-fluid">
+<div class="container">
     <div class="row shadow-lg p-3 mb-5 ColorPrincipal RadioDeBordes">
-
-        <div class="col-12 col-sm-6 col-md-5 col-lg-5">
-            <div class="accordion" id="cotizacion">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            COTIZADOR
-                        </button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#cotizacion">
-                        <div class="accordion-body">
-                            <script type="text/javascript">
-                                window.IXN_QUOTER_CONFIG = {
-                                    id: "fdb29d59-520c-455c-948a-b51b7defea14"
-                                }
-                            </script>
-                            <div id="ixn-website-quoter"></div>
-                            <script type="text/javascript" src="https://wq.ixn.tech/js/website_quoter.widget.js"></script>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <img class="img-fluid RadioDeBordes" src="app\views\assets\img\IMGCOTIZADORSALUD.webp">
+            <center>
+                <a type="button" class="btn btn-primary" href="https://www.healthsherpa.com/?_agent_id=freedom-asistentes" target="__blank">
+                    Cotizacion Vida
+                </a>
+            </center>
         </div>
-        <div class="col-12 col-sm-6 col-md-3 col-lg-3">
-            <blockquote class="text text-white" style="text-align: justify;">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <blockquote class="text text-white" style="text-align: justify">
                 <h4 class="text-center">
-                    OBTENGA UNA COTIZACIÓN DE SEGURO DE VIDA ASEQUIBLE
+                    OBTENGA UNA COTIZACIÓN DE SEGURO DE VIDA Y SALUD
                 </h4>
                 <p class="fs-5">
-                    Contamos con el mejor acompañamiento en cuanto a seguros de vida, ofrecemos los mejores servicios para beneficios de nuestros clientes. Ahorre tiempo buscando la mejor cotización de seguro de vida en línea en nuestro sitio. ¡Obtenga una cotización de seguro instantánea y ahórrese tiempo y dinero! Nuestro servicio instantáneo de cotización de seguros le permite acceder directamente a pólizas de seguro de vida de bajo costo de compañías de seguros de vida de alta calificación en todo el país.
+                    Contamos con el mejor acompañamiento en cuanto a seguros de vida y salud,
+                    ofrecemos los mejores servicios para beneficios de nuestros clientes.
+                    Ahorre tiempo buscando la mejor cotización de seguro de vida y de salud en línea
+                    en nuestro sitio. ¡Obtenga una cotización de seguro instantánea y
+                    ahórrese tiempo y dinero! Nuestro servicio instantáneo de cotización
+                    de seguros le permite acceder directamente a pólizas de seguro de vida y de salud
+                    de bajo costo de compañías de seguros de vida de alta calificación en
+                    todo el país.
                 </p>
             </blockquote>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-4 float-md-start">
-            <img class="img-fluid RadioDeBordes" src="app\views\assets\img\IMGCOTIZADOR.webp" alt="Select Insurance Cotizador" title="Cotiza tu seguro"><br>
+            <img class="img-fluid RadioDeBordes" src="app\views\assets\img\IMGCOTIZADORVIDA.webp" alt="Select Insurance Cotizador" title="Cotiza tu seguro" /><br />
+            <link href="https://wq.ixn.tech/css/reset.css" rel="stylesheet" type="text/css" />
+            <script type="text/javascript">
+                window.IXN_QUOTER_CONFIG = {
+                    id: "fdb29d59-520c-455c-948a-b51b7defea14",
+                };
+            </script>
+            <!-- Button trigger modal -->
+            <center>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Cotizacion Vida
+                </button>
+            </center>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body" id="ixn-website-quoter">
+                            <div id="ixn-website-quoter"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script type="text/javascript" src="https://wq.ixn.tech/js/website_quoter.widget.js"></script>
         </div>
     </div>
 </div>
